03fcf738a2c94e38bd2ffd3144e0d08ecc26e4c1
Nathan Rothenberger <nroth10@gmail.com>
2016-04-05 20:38:31 -0400

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var jade = require('gulp-jade')
+
+var sources = {
+  jade: './*.jade'
+}
+
+var destination = {
+  public: './'
+}
 
 // sass task
 gulp.task('styles', function() {
@@ -25,10 +34,6 @@ gulp.task('browser-sync', function() {
 });
 
 
-
-// test this
-
-
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
@@ -39,6 +44,7 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch('assets/css/*.scss', ['sass']);
+    gulp.watch(sources.jade, ['jade']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
@@ -50,4 +56,14 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('thing', ['serve']);
+
+
+// Compile and copy Jade
+gulp.task('jade', function(event) {
+  return gulp.src(sources.jade)
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest(destination.public))
+});
+
+
+gulp.task('thing', ['serve', 'jade']);

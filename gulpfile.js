6dfaed56e2d0ac5ebe93d97d880afda6685733e7
Nathan Rothenberger <nroth10@gmail.com>
2016-06-10 01:57:59 -0400

    Edit deploy task and .surgeignore

--- gulpfile.js
+++ gulpfile.js
@@ -6,71 +6,72 @@ var surge = require('gulp-surge')
 
 
 var sources = {
-  jade: './*.jade'
+	jade: './*.jade'
 }
 
 var destination = {
-  public: './'
+	public: './',
+	surge: 'full-screen-video-thing.surge.sh'
 }
 
 // sass task
 gulp.task('styles', function() {
-  return gulp.src('./assets/css/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+	return gulp.src('./assets/css/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./assets/css'));
 });
  
 // gulp.task('build', st)
 gulp.task('default', ['styles'], function() {
-  gulp.watch('assets/css/**/*.scss', ['styles']);
+	gulp.watch('assets/css/**/*.scss', ['styles']);
 })
 
 
 // browswer sync
 gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
+	browserSync.init({
+			server: {
+					baseDir: "./"
+			}
+	});
 });
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-  browserSync.init({
-      server: {
-        baseDir: "./"
-      }
-  });
+	browserSync.init({
+			server: {
+				baseDir: "./"
+			}
+	});
 
-  gulp.watch('assets/css/*.scss', ['sass']);
-  gulp.watch(sources.jade, ['jade']);
-  gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch('assets/css/*.scss', ['sass']);
+	gulp.watch(sources.jade, ['jade']);
+	gulp.watch("./*.html").on('change', browserSync.reload);
 
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("assets/css/**/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("assets/css"))
-        .pipe(browserSync.stream());
+		return gulp.src("assets/css/**/*.scss")
+				.pipe(sass())
+				.pipe(gulp.dest("assets/css"))
+				.pipe(browserSync.stream());
 });
 
 // Compile and copy Jade
 gulp.task('jade', function(event) {
-  return gulp.src(sources.jade)
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest(destination.public))
+	return gulp.src(sources.jade)
+		.pipe(jade({pretty: true}))
+		.pipe(gulp.dest(destination.public))
 });
 
 gulp.task('thing', ['serve', 'jade']);
 
 // Surge Deploy Task
 gulp.task('deploy', [], function () {
-  return surge({
-    project: destination.public,                // Path to your static build directory
-    domain: 'full-screen-video-thing.surge.sh'  // Your domain or Surge subdomain
-  })
+	return surge({
+		project: destination.public,  // Path to your static build directory
+		domain: destination.surge     // Your domain or Surge subdomain
+	})
 })
